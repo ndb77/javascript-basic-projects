78ba780b8e770293a8347e8202e9f2cdcdb7235a
ndb77 <bastolanoah@gmail.com>
2022-05-16 08:41:15 -0400

    up to navbar-4 complete

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -2,4 +2,16 @@
 // contains - checks classList for specific class
 // add - add class
 // remove - remove class
-// toggle - toggles class
+// toggle - toggles     class
+
+const navToggle = document.querySelector('.nav-toggle') // getting all elements that have this as their class
+const links = document.querySelector('.links') // getting all elements that have this as their class
+
+
+navToggle.addEventListener('click',function(){
+    links.classList.toggle('show-links') // toggling the css onto the elements containing .links
+                                         // note that the .links class has height to auto with overflow to hidden and the show-links set to 10.
+                                         // this allows the navbar to become unhidden when possible, and hides when text overflow happens
+    console.log(links.classList)
+})
+
