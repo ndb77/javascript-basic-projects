c594c08b3440ee84fac5bec64702ece4ff38b77a
ndb77 <bastolanoah@gmail.com>
2022-05-17 10:05:57 -0400

    08-menu with my solution for breakfast, and naive completed

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -73,15 +73,69 @@ const menuList = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 36.99,
+    img: "./images/item-10.jpeg",
+    desc: `yummy in my tummy steak`,
+  },
 ];
 
-const section_center = document.querySelector('.section-center')
+const section_center = document.querySelector('.section-center') // all of the menu items are encapulated by this class
+const filterBtns = document.querySelectorAll('.filter-btn') // selects all of the filter buttons
+// console.log(filterBtns)
 
+// load items
 window.addEventListener('DOMContentLoaded',function(){
   console.log(menuList)
   displayMenuItems(menuList);
   // console.log(displayMenu);
 });
+
+// filter items
+// adding an event listener to all filterBtns
+// when a filter button is clicked, the e.current.target.dataset.id is set to category
+// the menuCategory is built by filtering the menuList 
+filterBtns.forEach(function(filter_btn){
+  filter_btn.addEventListener('click',function(e){
+    console.log(e.currentTarget.dataset.id)
+    const category = e.currentTarget.dataset.id; // clicked category
+    const menuCategory = menuList.filter(function(menuItem){
+      // console.log(menuItem)
+      if(menuItem.category == category){
+        return menuItem
+      }
+    });
+    if(category=="all"){
+      displayMenuItems(menuList)
+    }else{
+      displayMenuItems(menuCategory)
+    }
+    console.log(menuCategory);
+  });
+});
+
+// filterBtns.forEach(function(filter_button){ // question is a value in the questionList 
+//   // console.log(filter_button);
+//   filter_button.addEventListener('click',function(){
+//     if(filter_button.innerText=="All"){
+//       console.log('All Clicked!')
+//     }
+//     if(filter_button.innerText=="Breakfast"){
+//       console.log('Breakfast Clicked!')
+//       displayBreakfast(menuList);
+//     }
+//     if(filter_button.innerText=="Lunch"){
+//       console.log('Lunch Clicked!')
+//     }
+//     if(filter_button.innerText=="Shakes"){
+//       console.log('Shakes Clicked!')
+//     }
+//   });
+  
+// });
 
 function displayMenuItems(menuItems){
   let displayMenu = menuItems.map(function(item){ // the iterator variable is every item in the menuList
@@ -96,7 +150,29 @@ function displayMenuItems(menuItems){
     </div>
   </article>`
     }); // converts the menuItem into formatted HTML
-    displayMenu = displayMenu.join(""); // formatted HTML is joined together
+    displayMenu = displayMenu.join(""); // the list with formatted HTML is joined together. 
     section_center.innerHTML = displayMenu; // sets the section_center to be the displayMenu
 }
 
+
+// my solution - for breakfast menu only, proof of concept
+
+// function displayBreakfast(menuItems){
+//   let displayBreakfastItems = menuItems.map(function(item){ // the iterator variable is every item in the menuList
+//     // Goal: Filter the menuItems list according to breakfast
+//       if(item.category=="breakfast"){
+//         return `<article class="menu-item">
+//           <img src=${item.img} class="photo" alt=${item.title}/>
+//           <div class="item-info">
+//             <header>
+//               <h4>${item.title}</h4>
+//               <h4 class="price">${item.price}</h4>
+//             </header>
+//             <p class="item-text">${item.desc}</p>
+//           </div>
+//         </article>`
+//       }
+//     });
+//     displayBreakfastItems = displayBreakfastItems.join(""); // the list with formatted HTML is joined together. 
+//     section_center.innerHTML = displayBreakfastItems; // sets the section_center to be the displayMenu
+// }
