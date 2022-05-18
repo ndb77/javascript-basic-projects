c34ba2640047df1d721ab2cd6aaec73136985b27
ndb77 <bastolanoah@gmail.com>
2022-05-18 14:04:52 -0400

    teacher solution added at top.

--- 11-tabs/setup/app.js
+++ 11-tabs/setup/app.js
@@ -1,26 +1,45 @@
-const about = document.querySelector('.about');
 const btns = document.querySelectorAll('.tab-btn'); // creates a nodelist of buttons
+const about = document.querySelector('.about');
 const articles = document.querySelectorAll('.content');
 
-btns.forEach(function(btn){
-    btn.addEventListener('click',function(e){
-        btn.classList.toggle('active');
-        console.log(btn.dataset.id);
-        btns.forEach(function(i){
-            if(btn.dataset.id!=i.dataset.id){
-                i.classList.remove('active');
-            }
+// this is a way to practice event targeting
+about.addEventListener('click',function(e){
+    // console.log(e.target.dataset.id)
+    const id = e.target.dataset.id;
+    if(id){
+        btns.forEach(function(btn){
+            btn.classList.remove('active');
+            e.target.classList.add('active')
         });
+        // hide other articles
         articles.forEach(function(article){
-            if(article.id==btn.dataset.id){
-                article.classList.add('active')
-            }else{
-                article.classList.remove('active')
-            }
+            article.classList.remove('active')
         })
-    });
-});
 
-articles.forEach(function(article){
-    console.log(article.id)
+        const element = document.getElementById(id); // getting element by id by passing id from e.target
+        element.classList.add("active");
+
+    }
 })
+
+
+// my solution
+// btns.forEach(function(btn){
+//     btn.addEventListener('click',function(e){
+//         btn.classList.toggle('active');
+//         console.log(btn.dataset.id);
+//         btns.forEach(function(i){
+//             if(btn.dataset.id!=i.dataset.id){
+//                 i.classList.remove('active');
+//             }
+//         });
+//         articles.forEach(function(article){
+//             if(article.id==btn.dataset.id){
+//                 article.classList.add('active')
+//             }else{
+//                 article.classList.remove('active')
+//             }
+//         })
+//     });
+// });
+
